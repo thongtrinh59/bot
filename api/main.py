5b251018eb18a1b032f0d3b676e354e832a9bd1f
Thomas <thongtrinh49@gmail.com>
2021-11-16 20:31:19 -0800

    temp change

--- api/main.py
+++ api/main.py
@@ -3,11 +3,13 @@ from flask import Flask, jsonify, abort, request, g
 import json
 import requests
 from flask_cors import CORS, cross_origin
+from wit import Wit
 
 
 import os
 import sys
 # from flask import request, g, jsonify
+from rivescript import RiveScript
 
 
 
@@ -17,11 +19,27 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+client = Wit("FXY4E2MNQBQ4VE3N3SUAIGJEJHOTYMRE")
+
+# rs = RiveScript()
+# rs.load_directory("./brain")
+# rs.sort_replies()
+
 @app.route('/')
 @cross_origin()
 def index():
     msg = request.args.get("message")
-    return jsonify({"message":msg})
+    resp = client.message(msg)
+    print(resp)
+    # reply = rs.reply("localuser", msg)
+    return jsonify({"message":'reply'})
+    
+    # if resp['intents'][0]['id'] == '579324833351112':
+    #     print(resp)
+    #     return jsonify({"message":"Hi, nice to meet you"})
+    # else:
+    #     print(resp['intents'][0]['id'])
+    #     return jsonify({"message":"msg"})
 
 
 
